10914448d720fdce83640e1e83d56931e8f43f3f
Carlos Daniel <danieldpaula07@gmail.com>
2026-05-26 18:27:28 -0300

    create financial manager menu

---
+++ main.py
@@ -0,0 +1,11 @@
+saldo = 0
+
+while True:
+
+    print("\n===== GERENCIADOR FINANCEIRO =====")
+    print("1 - Adicionar salário")
+    print("2 - Adicionar gasto")
+    print("3 - Ver saldo")
+    print("4 - Sair")
+
+    op = input("Escolha uma opção: ")
