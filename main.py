5a529f46dd7597c22f0be3b8ea6f395ffffe0b27
Carlos Daniel <danieldpaula07@gmail.com>
2026-05-26 18:41:42 -0300

    add expense system

--- main.py
+++ main.py
@@ -16,4 +16,29 @@ while True:
 
         saldo += valor
 
-    print(f"Salário de R${valor:.2f} adicionado!")
+        print(f"Salário de R${valor:.2f} adicionado!")
+
+    elif op == "2":
+
+        print("\nCategorias:")
+        print("1 - Comida")
+        print("2 - Transporte")
+        print("3 - Lazer")
+
+        categoria = input("Escolha a categoria: ")
+
+        valor = float(input("Digite o valor do gasto: "))
+
+        saldo -= valor
+
+        if categoria == "1":
+         print(f"Gasto com comida: R${valor:.2f}")
+
+        elif categoria == "2":
+            print(f"Gasto com transporte: R${valor:.2f}")
+
+        elif categoria == "3":
+            print(f"Gasto com lazer: R${valor:.2f}")
+
+        else:
+            print(f"Gasto de R${valor:.2f}")
